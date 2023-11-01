aa86cde58de1e8a70f845657cb8233e4396b3935
HenryChang6 <henry930619@gmail.com>
2023-11-01 21:54:45 +0800

    Cut the photo successfully

--- puzzle_game/puzzle_game/Form1.Designer.cs
+++ puzzle_game/puzzle_game/Form1.Designer.cs
@@ -38,8 +38,28 @@
             this.lbl_show = new System.Windows.Forms.Label();
             this.btn_init = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pb0 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackbar_diaplay)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -128,11 +148,100 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pb8);
+            this.panel1.Controls.Add(this.pb7);
+            this.panel1.Controls.Add(this.pb6);
+            this.panel1.Controls.Add(this.pb5);
+            this.panel1.Controls.Add(this.pb4);
+            this.panel1.Controls.Add(this.pb3);
+            this.panel1.Controls.Add(this.pb2);
+            this.panel1.Controls.Add(this.pb1);
+            this.panel1.Controls.Add(this.pb0);
+            this.panel1.Location = new System.Drawing.Point(76, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 8;
+            // 
+            // pb0
+            // 
+            this.pb0.Location = new System.Drawing.Point(0, 0);
+            this.pb0.Name = "pb0";
+            this.pb0.Size = new System.Drawing.Size(90, 90);
+            this.pb0.TabIndex = 0;
+            this.pb0.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(105, 0);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(90, 90);
+            this.pb1.TabIndex = 1;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(210, 0);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(90, 90);
+            this.pb2.TabIndex = 2;
+            this.pb2.TabStop = false;
+            // 
+            // pb5
+            // 
+            this.pb5.Location = new System.Drawing.Point(210, 105);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(90, 90);
+            this.pb5.TabIndex = 5;
+            this.pb5.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(105, 105);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(90, 90);
+            this.pb4.TabIndex = 4;
+            this.pb4.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(0, 105);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(90, 90);
+            this.pb3.TabIndex = 3;
+            this.pb3.TabStop = false;
+            // 
+            // pb8
+            // 
+            this.pb8.Location = new System.Drawing.Point(210, 210);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(90, 90);
+            this.pb8.TabIndex = 8;
+            this.pb8.TabStop = false;
+            // 
+            // pb7
+            // 
+            this.pb7.Location = new System.Drawing.Point(105, 210);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(90, 90);
+            this.pb7.TabIndex = 7;
+            this.pb7.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.Location = new System.Drawing.Point(0, 210);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(90, 90);
+            this.pb6.TabIndex = 6;
+            this.pb6.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(953, 471);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btn_init);
             this.Controls.Add(this.lbl_show);
             this.Controls.Add(this.lbl_hide);
@@ -146,6 +255,16 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackbar_diaplay)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,6 +281,16 @@
         private System.Windows.Forms.Label lbl_show;
         private System.Windows.Forms.Button btn_init;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb0;
     }
 }
 
--- puzzle_game/puzzle_game/Form1.cs
+++ puzzle_game/puzzle_game/Form1.cs
@@ -16,6 +16,7 @@ namespace puzzle_game
         int timer_min = 0, timer_second = 0;
         string timer_str_min, timer_str_second;
         int steps = 0;
+        Image current_img = null;
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +24,14 @@ namespace puzzle_game
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Game_Reset();
+        }
 
+        public void Game_Reset()
+        {
+            PictureBox[] pbs = { pb0, pb1, pb2, pb3, pb4, pb5, pb6, pb7};
+            foreach (PictureBox pb in pbs) pb.BackColor = Color.White;
+            current_img = null;
         }
 
         private void btn_choose_photo_Click(object sender, EventArgs e)
@@ -36,19 +44,75 @@ namespace puzzle_game
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     string filePath = openFileDialog.FileName;
-                    pictureBox1.Image = Image.FromFile(filePath);
+                    current_img = new Bitmap(Image.FromFile(filePath), new Size(270, 270));
+                    pictureBox1.Image = current_img;
                     imagePath = filePath;  // Assuming imagePath is a class-level variable
                     trackbar_diaplay.Value = 1;
                 }
             }
         }
+        public void CreatePuzzleBoard()
+        {
+            PictureBox[,] pbs = { { pb0, pb1, pb2 }, { pb3, pb4, pb5 }, { pb6, pb7, pb8 } };
+            List<Image> pieces = CutImageIntoPieces(current_img);
+            //Hide One piece
+            Random rnd = new Random();
+            int hidden_index = rnd.Next(0, 9);
+            pieces.RemoveAt(hidden_index);
+            //Add another 8 into PictureBox
+            List<int> indices = Enumerable.Range(0,8).ToList();
+            for(int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i == 2 && j == 2) 
+                    { 
+                        pbs[i, j].Image = null;
+                        break;
+                    } 
+                    int rnd_index = rnd.Next(indices.Count);
+                    int target_index = indices[rnd_index];
+                    pbs[i,j].Image = pieces[target_index];
+                    indices.RemoveAt(rnd_index);
+                }
+            }
+        }
+
+        public List<Image> CutImageIntoPieces(Image img)
+        {
+            List<Image> pieces = new List<Image>();
+            int piece_height = 90, piece_width = 90;
+            for(int r = 0; r < 3; r++)
+            {
+                for(int c = 0; c < 3; c++) 
+                { 
+                
+                    //syntax: Rectangle(x_cor, y_cor, width, height)
+                    Rectangle rec = new Rectangle(piece_width * c, piece_height * r , piece_width, piece_height);
+                    Bitmap piece = new Bitmap(piece_width, piece_height);
+                    using (Graphics g = Graphics.FromImage(piece))
+                    {
+                        g.DrawImage(img, 0, 0, rec, GraphicsUnit.Pixel);
+                    }
+                    pieces.Add(piece);
+                }
+            }
+            return pieces;
+        }
 
         private void btn_init_Click(object sender, EventArgs e)
         {
+            if (current_img == null)
+            {
+                MessageBox.Show("請先選擇圖片!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
             // timer initialization
             timer1.Enabled = true; timer_min = 0; timer_second = 0;
             // steps initialization
             steps = 0; lbl_steps.Text = $"移動步數:0";
+            // puzzle board initialization
+            CreatePuzzleBoard();
         }
         public void Update_Steps()
         {
@@ -75,7 +139,7 @@ namespace puzzle_game
 
         private void trackbar_diaplay_ValueChanged(object sender, EventArgs e)
         {
-            pictureBox1.Image = trackbar_diaplay.Value == 0 ? null : Image.FromFile(imagePath);
+            if(current_img != null) pictureBox1.Image = trackbar_diaplay.Value == 0 ? null : current_img;
         }
     }
 }
